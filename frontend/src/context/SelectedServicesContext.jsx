d26bb3070d9a42b8fe2ca75bae9c1dbc37e712ca
fouad gabr 17 <foadjabr14@gmail.com>
2024-10-24 11:35:24 +0300

    remove comments

--- frontend/src/context/SelectedServicesContext.jsx
+++ frontend/src/context/SelectedServicesContext.jsx
@@ -4,11 +4,11 @@ export const SelectedServicesContext = createContext();
 
 export const SelectedServicesProvider = ({ children }) => {
   const [selectedServices, setSelectedServices] = useState([]);
-  const [totalCost, setTotalCost] = useState(0); // Add totalCost state
+  const [totalCost, setTotalCost] = useState(0);
 
   return (
     <SelectedServicesContext.Provider
-      value={{ selectedServices, setSelectedServices, totalCost, setTotalCost }} // Provide totalCost and its setter
+      value={{ selectedServices, setSelectedServices, totalCost, setTotalCost }}
     >
       {children}
     </SelectedServicesContext.Provider>
